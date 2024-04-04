85289f468c79fd09aa596847bebb64e56f08048d
Rodrigo oliveira <98712381+1stRodrigo@users.noreply.github.com>
2024-04-04 20:24:44 -0300

    Add files via upload
    manutenção

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AuthProvider from './src/contexts/auth';
 import Routes from './src/routes';
 
 export default function App(){
     return(
         <NavigationContainer>
-            <Routes/>
+            <AuthProvider>
+                <Routes/>
+            </AuthProvider>
         </NavigationContainer>
     )
 }
